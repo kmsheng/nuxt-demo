637d0f9f931b63b562ca7b20ccf5e77cef63b939
kmsheng <kmsh3ng@gmail.com>
2017-08-30 15:34:56 +0800

    add todos route

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 
 import users from './users';
+import todos from './todos';
 
 const router = Router();
 
 // Add USERS Routes
 router.use(users);
+router.use(todos);
 
 export default router;
