ce76d57888a33a279d15922c44fe090a0f20fd76
kmsheng <kmsh3ng@gmail.com>
2017-08-30 15:45:33 +0800

    new file pages/todos.vue

---
+++ pages/todos.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1>Todo List</h1>
+    <ul class="todos">
+      <li>item</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    const {data} = await axios.get('/api/todos');
+    return {todos: data};
+  },
+  head () {
+    return {
+      title: 'Todos'
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
