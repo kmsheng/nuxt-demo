b3187fb2220c499681760575bca1133daf7d36b9
kmsheng <kmsh3ng@gmail.com>
2017-08-30 15:26:05 +0800

    add todos link

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <section class="container">
     <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <ul class="users">
+    <ul class="links">
       <li>
         <nuxt-link :to="'users'">Users Demo</nuxt-link>
+        <nuxt-link :to="'todos'">Todo List Demo</nuxt-link>
       </li>
     </ul>
   </section>
